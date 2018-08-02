9d6f637e1b9ba780971237350f88b216840dbe99
Simon Kiflay <szkiflay@gmail.com>
2018-08-02 11:10:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var greeting = "Hello, everybody!";
 var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = greeting + specialGuest + "!";
+`Hello, ${greetSpecialGuest}!`;
 
 var topic = "let's talk about Birthday";
 
 var conversation = `${topic}`;
-`${greetSpecialGuest}!`;
